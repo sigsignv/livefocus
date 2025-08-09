bf45e6f34219a733863c899632323e44a11c5594
Sigsign <sig@signote.cc>
2025-08-09 18:31:54 +0900

    Update packages: wxt to v0.20.7

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
   manifest: {
     permissions: ['activeTab', 'scripting'],
   },
